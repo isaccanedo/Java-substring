3c90fcdafd5683c031e13642657a7586d47e5add
isaccanedo <isaccanedo@gmail.com>
2021-01-12 09:36:56 -0300

    :confetti_ball: Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+	
+	/*
+	 *  Given the string "strawberries" saved in a variable called fruit, what would fruit.substring(2, 5) return?
+	 *  (Dada a string "strawberries" salva em uma variável chamada fruit, o que fruit.substring (2, 5) retornaria?)
+	 *  
+	 *   1 - raw == correct
+	 *   2 - awb
+	 *   3 - rawb
+	 *   4 - traw
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		
+		String fruit = "strawberries";
+		
+		System.out.println(fruit.substring(2, 5));
+
+		
+		
+	}
+
+}
